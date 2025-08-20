156c3f17feda911f582b03341a76e103e9e859c1
sjhub <sj.hub9796@gmail.com>
2025-08-21 07:57:01 +0900

    set correct inject target

--- c2me_opts_scheduling/src/main/java/com/ishland/c2me/opts/scheduling/mixin/idle_tasks/autosave/enhanced_autosave/MixinWorldChunk.java
+++ c2me_opts_scheduling/src/main/java/com/ishland/c2me/opts/scheduling/mixin/idle_tasks/autosave/enhanced_autosave/MixinWorldChunk.java
@@ -1,11 +1,13 @@
 package com.ishland.c2me.opts.scheduling.mixin.idle_tasks.autosave.enhanced_autosave;
 
 import com.ishland.c2me.opts.scheduling.common.idle_tasks.IThreadedAnvilChunkStorage;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.chunk.LevelChunkSection;
@@ -16,6 +18,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LevelChunk.class)
 public abstract class MixinWorldChunk extends ChunkAccess {
@@ -24,7 +27,7 @@ public abstract class MixinWorldChunk extends ChunkAccess {
         super(pos, upgradeData, heightLimitView, biome, inhabitedTime, sectionArrayInitializer, blendingData);
     }
 
-    @Inject(method = "*", at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/chunk/ChunkAccess;unsaved:Z"))
+    @Inject(method = "<init>(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ProtoChunk;Lnet/minecraft/world/level/chunk/LevelChunk$PostLoadProcessor;)V", at = @At(value = "TAIL"))
     private void onSetShouldSave(CallbackInfo ci) {
         //noinspection ConstantConditions
         if (this.unsaved && (Object) this instanceof LevelChunk worldChunk) {
@@ -34,4 +37,13 @@ public abstract class MixinWorldChunk extends ChunkAccess {
         }
     }
 
+    @Inject(method = "setBlockState", at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/chunk/LevelChunk;unsaved:Z"))
+    private void onSetShouldSave0(BlockPos p_62865_, BlockState p_62866_, boolean p_62867_, CallbackInfoReturnable<BlockState> cir) {
+        //noinspection ConstantConditions
+        if (this.unsaved && (Object) this instanceof LevelChunk worldChunk) {
+            if (worldChunk.getLevel() instanceof ServerLevel serverWorld) {
+                ((IThreadedAnvilChunkStorage) serverWorld.getChunkSource().chunkMap).enqueueDirtyChunkPosForAutoSave(this.getPos());
+            }
+        }
+    }
 }
